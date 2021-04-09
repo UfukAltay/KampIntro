244e6e322d11c6b2d811dd0dd500afd0f36fef33
Ufuk ALTAY <ufk.altay@gmail.com>
2021-04-09 23:38:44 +0300

    5. Hafta ödevi Game Center Projesi

---
+++ m-GameCenterProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace m_GameCenterProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
