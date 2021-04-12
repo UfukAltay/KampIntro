51a7123148e4c0fac1fc66600ee71697f91488a9
Ufuk ALTAY <ufk.altay@gmail.com>
2021-04-12 23:35:27 +0300

    5. Hafta ödevi Game Center Projesi güncelleme

---
+++ m-GameCenterProject/Abstracts/IGameService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace m_GameCenterProject.Abstracts
+{
+    public interface IGameService
+    {
+        void Add();
+        void Delete();
+        void Update();
+    }
+}
---
+++ m-GameCenterProject/Abstracts/IGamerService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace m_GameCenterProject.Abstracts
+{
+    public interface IGamerService
+    {
+        void Add();
+        void Delete();
+        void Update();
+    }
+}
